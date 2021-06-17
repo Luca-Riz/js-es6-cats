8620b1f56f0c8c675ff1499723a5e24e3bcd0bb0
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-17 14:39:54 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Milestone 1 Definire un array di oggetti; ogni oggetto rappresenta un gatto, che è caratterizzato da: nome, età, colore e sesso.
+// Tramite il forEach(), stampare in pagina tutti i gattini, ciascuno con il proprio colore e il proprio nome.
+
+
+
+// Milestone 2 Dividere i gatti in due contenitori distinti in base al sesso e aggiungere a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu, se maschio. Il colore del fiocco deve essere più tenue se il gatto è più giovane, più scuro se il gatto è più vecchio.
+
+
+
+// Milestone 3 Creare un nuovo array con prima tutti i gattini femmina e poi tutti i gattini maschio, inserendo solamente nome, colore e opacità del fiocco per ogni gatto.
+// Fate la prima Milestone da soli e poi attendete in comunicazioni l'orario del collegamento.
