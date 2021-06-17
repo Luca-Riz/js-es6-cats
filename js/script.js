9ef65230a5d5e5ec1a5fb276a1db3ea926888d07
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-17 15:03:13 +0200

    add milestone 1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,59 @@
 // Milestone 1 Definire un array di oggetti; ogni oggetto rappresenta un gatto, che è caratterizzato da: nome, età, colore e sesso.
 // Tramite il forEach(), stampare in pagina tutti i gattini, ciascuno con il proprio colore e il proprio nome.
 
+const gatti = [
+    {
+        nome: 'Pallina',
+        eta: 0.5,
+        colore: '#bb3b3b',
+        sesso: 'female'
+    },
+    {
+        nome: 'Pallino',
+        eta: 0.7,
+        colore: '#ac3f3f',
+        sesso: 'male'
+    },
+    {
+        nome: 'Micio',
+        eta: 0.3,
+        colore: '#946a1e',
+        sesso: 'male'
+    },
+    {
+        nome: 'Micia',
+        eta: 0.4,
+        colore: '#bb8e3b',
+        sesso: 'female'
+    },
+    {
+        nome: 'Luna',
+        eta: 0.5,
+        colore: '#ac6718ee',
+        sesso: 'female'
+    },
+    {
+        nome: 'Chicco',
+        eta: 0.9,
+        colore: '#3d2b09',
+        sesso: 'male'
+    },
+    {
+        nome: 'Trilly',
+        eta: 0.1,
+        colore: '#2c2009',
+        sesso: 'female'
+    }
+];
 
+gatti.forEach((element) => {
+    const {nome, colore} = element;
+    console.log(
+        `
+        ${nome} è di colore ${colore}
+        `
+    );
+});
 
 // Milestone 2 Dividere i gatti in due contenitori distinti in base al sesso e aggiungere a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu, se maschio. Il colore del fiocco deve essere più tenue se il gatto è più giovane, più scuro se il gatto è più vecchio.
 
